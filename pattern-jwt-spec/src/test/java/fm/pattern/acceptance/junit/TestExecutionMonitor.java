469507e2e87b298af826ea712112708ff5bbb226
Brandon Grenier <brandon.john.grenier@gmail.com>
2017-03-09 21:23:55 +1100

    Added jacoco and overalls plugins

--- pattern-jwt-spec/src/test/java/fm/pattern/acceptance/junit/TestExecutionMonitor.java
+++ pattern-jwt-spec/src/test/java/fm/pattern/acceptance/junit/TestExecutionMonitor.java
@@ -1,15 +1,18 @@
 package fm.pattern.acceptance.junit;
 
-
 public class TestExecutionMonitor {
 
 	private static Integer testClassesToRun = 0;
 	private static Integer testClassesExecuted = 0;
-	
+
+	private TestExecutionMonitor() {
+
+	}
+
 	public static void testStarted() {
 		testClassesToRun += 1;
 	}
-	
+
 	public static void testCompleted() {
 		testClassesExecuted += 1;
 	}
@@ -17,5 +20,5 @@ public class TestExecutionMonitor {
 	public static boolean allTestsHaveRun() {
 		return testClassesToRun == testClassesExecuted;
 	}
-	
+
 }
