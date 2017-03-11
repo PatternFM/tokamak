83a09bf23e8da261a7635f32ad2d6fd98ca8bd8d
Brandon Grenier <brandon.john.grenier@gmail.com>
2017-03-11 21:42:14 +1100

    Added automated acceptance testing library, and deployment/release scripts.

--- pattern-jwt-server/start.sh
+++ pattern-jwt-server/start.sh
@@ -3,5 +3,5 @@
 mvn install -DskipTests=true
 
 # TODO: Remove hard coded profile.
-nohup mvn spring-boot:run -Dspring.profiles.active=ci >/dev/null 2>&1 &
-# nohup mvn spring-boot:run -Dspring.profiles.active= >/dev/null 2>&1 &
+# nohup mvn spring-boot:run -Dspring.profiles.active=ci >/dev/null 2>&1 &
+nohup mvn spring-boot:run -Dspring.profiles.active= >/dev/null 2>&1 &
