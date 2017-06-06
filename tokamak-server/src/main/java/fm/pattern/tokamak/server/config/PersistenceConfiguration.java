371bba37930ca84dd3de0b09171cd650b1165140
Brandon Grenier <brandon.john.grenier@gmail.com>
2017-06-06 23:40:00 +1000

    Initial configuration to use Spring Data JPA

--- tokamak-server/src/main/java/fm/pattern/tokamak/server/config/PersistenceConfiguration.java
+++ tokamak-server/src/main/java/fm/pattern/tokamak/server/config/PersistenceConfiguration.java
@@ -16,83 +16,11 @@
 
 package fm.pattern.tokamak.server.config;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableConfigurationProperties(DatabaseProperties.class)
 @EnableTransactionManagement
 public class PersistenceConfiguration {
-
-	private static final String MODEL_PACKAGE = "fm.pattern.tokamak";
-
-	@Autowired
-	private DatabaseProperties databaseProperties;
-
-	@DependsOn("flyway")
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-		em.setDataSource(dataSource());
-		em.setPackagesToScan(new String[] { MODEL_PACKAGE });
-
-		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-		em.setJpaVendorAdapter(vendorAdapter);
-		em.setJpaProperties(getHibernateProperties());
-
-		return em;
-	}
-
-	@Bean(name = "dataSource")
-	public DataSource dataSource() {
-		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(databaseProperties.getDriver());
-		dataSource.setUrl(databaseProperties.getUrl() + "/" + databaseProperties.getSchema() + "?useSSL=false&requireSSL=false");
-		dataSource.setUsername(databaseProperties.getUsername());
-		dataSource.setPassword(databaseProperties.getPassword());
-		dataSource.setValidationQuery("SELECT 1");
-		dataSource.setTestOnBorrow(true);
-		dataSource.setTestOnReturn(true);
-		return dataSource;
-	}
-
-	@Bean(name = "flyway")
-	public Flyway flyway() {
-		Flyway flyway = new Flyway();
-		flyway.setSchemas(databaseProperties.getSchema());
-		flyway.setDataSource(databaseProperties.getUrl() + "?useSSL=false&requireSSL=false", databaseProperties.getUsername(), databaseProperties.getPassword());
-		flyway.setLocations(databaseProperties.getMigrations().split(","));
-		if (databaseProperties.shouldMigrate()) {
-			flyway.migrate();
-		}
-		return flyway;
-	}
-
-	@Bean(name = "transactionManager")
-	public PlatformTransactionManager transactionManager() {
-		return new JpaTransactionManager();
-	}
-
-	private Properties getHibernateProperties() {
-		Properties properties = new Properties();
-		properties.setProperty("hibernate.dialect", databaseProperties.getDialect());
-		properties.setProperty("javax.persistence.validation.mode", "none");
-		return properties;
-	}
 
 }
